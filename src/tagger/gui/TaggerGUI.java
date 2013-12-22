01a4f258355be2dfe636143fb75a27cec490b7f7
JustinToniazzo <jjtoniaz@ncsu.edu>
2013-12-21 22:49:22 -0500

    Window location and size are now preserved between program runs.

--- src/tagger/gui/TaggerGUI.java
+++ src/tagger/gui/TaggerGUI.java
@@ -1,31 +1,55 @@
 package tagger.gui;
 
 import tagger.core.TrackGetter;
-import tagger.core.Tracklist;
 
 import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.KeyAdapter;
-import java.awt.event.KeyEvent;
+import java.awt.event.*;
+import java.util.prefs.Preferences;
 
 /**
  * Author: Justin Toniazzo
  * Date:   14 December 2013
  */
-public class TaggerGUI implements ActionListener {
+public class TaggerGUI extends JFrame implements ActionListener {
     private JTextField enterAURLTextField;
     private JTextArea textArea1;
     private JPanel panel;
 
+    private Preferences preferences;
     private TrackGetter trackGetter;
 
     public TaggerGUI() {
+        super("Tagger");
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        setContentPane(panel);
         enterAURLTextField.addKeyListener(new KeyAdapter() {
             public void keyTyped(KeyEvent e) {
                 String url = enterAURLTextField.getText();
                 if(TrackGetter.isValidURL(url))
                     getTracks(url);
+            }
+        });
+
+        // Load or save preferences.
+        preferences = Preferences.userRoot().node(getClass().getName());
+        addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                preferences.put("winPosX", getLocation().getX() + "");
+                preferences.put("winPosY", getLocation().getY() + "");
+                preferences.put("winHeight", getSize().getHeight() + "");
+                preferences.put("winWidth", getSize().getWidth() + "");
+            }
+
+            @Override
+            public void windowOpened(WindowEvent e) {
+                int x = (int) Double.parseDouble(preferences.get("winPosX", "200"));
+                int y = (int) Double.parseDouble(preferences.get("winPosY", "200"));
+                setLocation(x, y);
+
+                int height = (int) Double.parseDouble(preferences.get("winHeight", getPreferredSize().getHeight() + ""));
+                int width = (int) Double.parseDouble(preferences.get("winWidth", getPreferredSize().getWidth() + ""));
+                setSize(width, height);
             }
         });
     }
@@ -53,9 +77,7 @@ public class TaggerGUI implements ActionListener {
             System.err.println(e.getClass().getName() + ": " + e.getMessage());
         }
 
-        JFrame frame = new JFrame("Tagger");
-        frame.setContentPane(new TaggerGUI().panel);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        JFrame frame = new TaggerGUI();
         frame.pack();
         frame.setVisible(true);
     }
